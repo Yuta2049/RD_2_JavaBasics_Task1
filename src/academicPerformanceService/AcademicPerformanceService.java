ad84e5faf31d6aab2453ccec9a663fc2bd23b6f2
Yuta2049 <vinci2049@gmail.com>
2018-11-13 10:52:52 +0400

    Version 0.2

--- src/academicPerformanceService/AcademicPerformanceService.java
+++ src/academicPerformanceService/AcademicPerformanceService.java
@@ -1,4 +1,31 @@
 package academicPerformanceService;
 
+import data.Student;
+
 public class AcademicPerformanceService implements IAcademicPerformanceService {
+
+    @Override
+    public int getDaysToEndOfCurriculum(Student student) {
+        return 0;
+    }
+
+    @Override
+    public int getAverageMark(Student student) {
+        return 0;
+    }
+
+    @Override
+    public void printPossibilityOfExpulsion(Student student) {
+
+    }
+
+    @Override
+    public void printListOfStudents(boolean sort) {
+
+    }
+
+    @Override
+    public void printListOfStudentsPossibleWontBeExpelled() {
+
+    }
 }
--- src/academicPerformanceService/IAcademicPerformanceService.java
+++ src/academicPerformanceService/IAcademicPerformanceService.java
@@ -1,4 +1,17 @@
 package academicPerformanceService;
 
+import data.Student;
+
 public interface IAcademicPerformanceService {
+
+    int getDaysToEndOfCurriculum(Student student);
+
+    int getAverageMark(Student student);
+
+    void printPossibilityOfExpulsion(Student student);
+
+    void printListOfStudents(boolean sort);
+
+    void printListOfStudentsPossibleWontBeExpelled();
+
 }
